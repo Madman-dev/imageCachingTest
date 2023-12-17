2592ed86be5d42b9734e30c97a7e191b80b73759
7과11사이 <dongjunlee222@gmail.com>
2023-12-17 23:38:12 +0900

    [FEAT]: loading image with animation

--- ImageCaching/ImageCaching/ImageLoad.swift
+++ ImageCaching/ImageCaching/ImageLoad.swift
@@ -17,30 +17,36 @@ class ImageLoad: UIViewController {
     @IBAction func fetchImageTapped(_ sender: UIButton) {
         guard let url = URL(string: "https://picsum.photos/1000") else { return }
         
-        // 1. url을 데이터로 변환
-        guard let data = try? Data(contentsOf: url) else { return }
+        UIView.animate(withDuration: 2) {
+            self.imageView.transform = CGAffineTransform(rotationAngle: .pi)    // when trying to rotate newly loaded image, delay occurs. Need to download new image - locks UI in place.
+        }
         
-        //MARK: - loading actual size of the image
-        /*
-         Before loading image - memory usage = 24.8mb
-         Actual image size being 1000 * 1000 - memory usage = 30.8mb
-         */
-        
-        // imageView.image = UIImage(data: data)
-        
-        //MARK: - loading image using smaller image
-        /*
-         currently loaded image size 300 * 300 - memory usage = 28.6 ~ 7mb
-         currently loaded image size 100 * 100 - memory usage = 28.1
-         */
-        
-        let image = UIImage(data: data)
-        
-        image?.prepareThumbnail(of: CGSize(width: 100, height: 100),
-                                completionHandler: { image in
-            DispatchQueue.main.async {
-                self.imageView.image = image
-            }
-        })
+        DispatchQueue.global().async {  // enables locked UI to continue loading along with the animation
+            // 1. url을 데이터로 변환
+            guard let data = try? Data(contentsOf: url) else { return }
+            
+            //MARK: - loading actual size of the image
+            /*
+             Before loading image - memory usage = 24.8mb
+             Actual image size being 1000 * 1000 - memory usage = 30.8mb
+             */
+            
+            // imageView.image = UIImage(data: data)
+            
+            //MARK: - loading image using smaller image
+            /*
+             currently loaded image size 300 * 300 - memory usage = 28.6 ~ 7mb
+             currently loaded image size 100 * 100 - memory usage = 28.1
+             */
+            
+            let image = UIImage(data: data)
+            
+            image?.prepareThumbnail(of: CGSize(width: 100, height: 100),
+                                    completionHandler: { image in
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            })
+        }
     }
 }
